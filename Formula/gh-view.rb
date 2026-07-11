b3f2884a18af9a82c89581bf48aaca95889ff33b
nikita.ivanov <nikitaivanovv.ff@gmail.com>
2026-07-11 18:46:45 +0400

    v0.0.5

--- Formula/gh-view.rb
+++ Formula/gh-view.rb
@@ -1,15 +1,15 @@
 class GhView < Formula
   desc "Terminal view for GitHub pull requests"
   homepage "https://github.com/nikitaivanovvff/gh-view"
-  version "0.0.4"
+  version "0.0.5"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/nikitaivanovvff/gh-view/releases/download/v0.0.4/gh-view-v0.0.4-aarch64-apple-darwin.tar.gz"
-    sha256 "8b34ff7545502800dc3191deff733aa7551582ac63ad7e881952ef20bcce95f2"
+    url "https://github.com/nikitaivanovvff/gh-view/releases/download/v0.0.5/gh-view-v0.0.5-aarch64-apple-darwin.tar.gz"
+    sha256 "7d07f634c69db8f21fd65f2a0c87baa52b538ae24f2f3c0986ceb771cc8486ee"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/nikitaivanovvff/gh-view/releases/download/v0.0.4/gh-view-v0.0.4-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c8861de9a9d02012c23e105937281f054d7e4468c269f83b67b6e4747f2c5037"
+    url "https://github.com/nikitaivanovvff/gh-view/releases/download/v0.0.5/gh-view-v0.0.5-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "e2b461e726cb56a9022f971da47a3c3db0392a4d76211a94e20e07d21c87a554"
   end
 
   depends_on "gh"
